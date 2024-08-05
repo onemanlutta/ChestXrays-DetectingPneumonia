13481a8ccfbddc206c78502d7d6594989aa844a3
maerolutta <118937365+onemanlutta@users.noreply.github.com>
2024-08-05 12:10:18 -0500

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def predict_pneumonia(img):
     img_array = image.img_to_array(img) / 255.0
     img_array = np.expand_dims(img_array, axis=0)
     prediction = model.predict(img_array)
-    result = 'Pneumonia Infection' if prediction > 0.5 else 'Normal Radiograph'
+    result = 'Pneumonia Infection' if prediction > 0.9 else 'Normal Radiograph'
     accuracy = prediction[0][0] if prediction > 0.9 else 1 - prediction[0][0]
     return result, accuracy
 
